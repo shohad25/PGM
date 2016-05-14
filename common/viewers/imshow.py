25fa5d06628c99f7b0521dd3cd4d96e0b4c7d249
shohad25 <shohad25@gmail.com>
2016-05-14 14:44:32 +0300

    A. Update plot_letters(): 	1. Add regular CRF 	2. Compare to chain CRF B. Add common dir + viewers.imshow

---
+++ common/viewers/imshow.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def imshow(image, colormap='gray', ax=None, fig=None, block=False):
+    """
+    Image show
+    :param image: image to show
+    :param colormap: colormap
+    :param ax: axes handle
+    :param fig: figure
+    :param block: block shell
+    :return: plot
+    """
+    if ax and fig:
+        # For subplots
+        img = ax.imshow(image, interpolation="none", cmap=plt.get_cmap(colormap))
+        div = make_axes_locatable(ax)
+        cax = div.append_axes("right", size="10%", pad=0.05)
+        plt.colorbar(img, cax=cax)
+        plt.show(block=block)
+    else:
+        # For only one plot, good for interactive mode
+        plt.imshow(image, interpolation="none", cmap=plt.get_cmap(colormap))
+        plt.colorbar()
+        plt.show(block=block)
--- plot_letters.py
+++ plot_letters.py
@@ -32,15 +32,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.svm import LinearSVC
-
+from common.viewers.imshow import imshow
 from pystruct.datasets import load_letters
 from pystruct.models import ChainCRF
 from pystruct.learners import FrankWolfeSSVM
 
 abc = "abcdefghijklmnopqrstuvwxyz"
 
+# Load data:
 letters = load_letters()
 X, y, folds = letters['data'], letters['labels'], letters['folds']
+
 # we convert the lists to object arrays, as that makes slicing much more
 # convenient
 X, y = np.array(X), np.array(y)
@@ -50,17 +52,25 @@ y_train, y_test = y[folds == 1], y[folds != 1]
 # Train linear SVM
 svm = LinearSVC(dual=False, C=.1)
 # flatten input
+
 svm.fit(np.vstack(X_train), np.hstack(y_train))
 
-# Train linear chain CRF
+# Train CRF
 model = ChainCRF()
 ssvm = FrankWolfeSSVM(model=model, C=.1, max_iter=11)
-ssvm.fit(X_train, y_train)
 
-print("Test score with chain CRF: %f" % ssvm.score(X_test, y_test))
+ssvm.fit(np.vstack(X_train).reshape((5375, 1, 128)), np.hstack(y_train).reshape(5375, 1))
+
+# Train linear chain CRF
+chain_model = ChainCRF()
+chain_ssvm = FrankWolfeSSVM(model=chain_model, C=.1, max_iter=11)
+chain_ssvm.fit(X_train, y_train)
 
 print("Test score with linear SVM: %f" % svm.score(np.vstack(X_test),
                                                    np.hstack(y_test)))
+print("Test score with CRF: %f" % ssvm.score(X_test, y_test))
+
+print("Test score with Linear Chain CRF: %f" % chain_ssvm.score(X_test, y_test))
 
 # plot some word sequenced
 n_words = 4
@@ -71,13 +81,16 @@ fig, axes = plt.subplots(n_words, max_word_len, figsize=(10, 10))
 fig.subplots_adjust(wspace=0)
 for ind, axes_row in zip(selected, axes):
     y_pred_svm = svm.predict(X_test[ind])
-    y_pred_chain = ssvm.predict([X_test[ind]])[0]
-    for i, (a, image, y_true, y_svm, y_chain) in enumerate(
-            zip(axes_row, X_test[ind], y_test[ind], y_pred_svm, y_pred_chain)):
+    y_pred_chain = chain_ssvm.predict([X_test[ind]])[0]
+    y_pred_crf = ssvm.predict([X_test[ind]])[0]
+
+    for i, (a, image, y_true, y_svm, y_chain, y_crf) in enumerate(
+            zip(axes_row, X_test[ind], y_test[ind], y_pred_svm, y_pred_chain, y_pred_crf)):
         a.matshow(image.reshape(16, 8), cmap=plt.cm.Greys)
         a.text(0, 3, abc[y_true], color="#00AA00", size=25)
         a.text(0, 14, abc[y_svm], color="#5555FF", size=25)
         a.text(5, 14, abc[y_chain], color="#FF5555", size=25)
+        a.text(5, 3, abc[y_crf], color="#FF5551", size=25)
         a.set_xticks(())
         a.set_yticks(())
     for ii in range(i + 1, max_word_len):
