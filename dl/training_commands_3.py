6dc011a1069bb44934df6eeceddf9c8e1bfa36f2
shohad25 <shohad25@gmail.com>
2016-06-14 10:03:07 +0300

    Adding training script using network3 class

--- dl/training_commands_3.py
+++ dl/training_commands_3.py
@@ -13,6 +13,7 @@ import theano.tensor as T
 def ReLU(z): return T.maximum(0.0, z)
 from theano.tensor.nnet import sigmoid
 from theano.tensor import tanh
+from dl.user_network import training_results_handler
 
 # get the data
 letters = load_letters()
@@ -45,33 +46,42 @@ mini_batch_size = np.int64(10)
 # net = Network([image_size, 128, num_of_labels])
 # conv_layer = ConvPoolLayer(filter_shape=(4, 1, 3, 3),
 #                            image_shape=(mini_batch_size, 1, 16, 8), poolsize=(2, 2), activation_fn=ReLU)
-# fc_layer = FullyConnectedLayer(128, 256, activation_fn=ReLU, p_dropout=0.0)
-# fc_layer2 = FullyConnectedLayer(256, 128, activation_fn=ReLU, p_dropout=0.0)
-fc_layer = FullyConnectedLayer(128, 256, activation_fn=sigmoid, p_dropout=0.0)
-fc_layer2 = FullyConnectedLayer(256, 128, activation_fn=sigmoid, p_dropout=0.0)
+fc_layer = FullyConnectedLayer(128, 256, activation_fn=sigmoid, p_dropout=0.2)
+fc_layer1 = FullyConnectedLayer(256, 512, activation_fn=sigmoid, p_dropout=0.2)
+fc_layer2 = FullyConnectedLayer(512, 256, activation_fn=sigmoid, p_dropout=0.2)
+fc_layer3 = FullyConnectedLayer(256, 128, activation_fn=sigmoid, p_dropout=0.2)
 sm_layer = SoftmaxLayer(128, 26)
 # net = Network(layers=[conv_layer, fc_layer, fc_layer2, sm_layer], mini_batch_size=mini_batch_size)
-net = Network(layers=[fc_layer, fc_layer2, sm_layer], mini_batch_size=mini_batch_size)
+# net = Network(layers=[fc_layer, fc_layer2, sm_layer], mini_batch_size=mini_batch_size)
+net = Network(layers=[fc_layer, fc_layer1, fc_layer2, fc_layer3, sm_layer], mini_batch_size=mini_batch_size)
 # train NN:
-num_of_epochs = 50
+num_of_epochs = 1000
 
 train_set_x = theano.shared(np.asarray(X_train, dtype=theano.config.floatX))
 train_set_y = theano.shared(np.asarray(y_train, dtype=theano.config.floatX))
 test_set_x = theano.shared(np.asarray(X_test, dtype=theano.config.floatX))
 test_set_y = theano.shared(np.asarray(y_test, dtype=theano.config.floatX))
 
+# train_set_x = theano.shared(value=X_train)
+# test_set_x = theano.shared(value=X_test)
+# test_set_x = theano.shared(np.asarray(X_test, dtype=theano.config.floatX))
+
+train_set_y = theano.shared(np.asarray(y_train, dtype=theano.config.floatX))
+test_set_y = theano.shared(np.asarray(y_test, dtype=theano.config.floatX))
+
+
 train_set_y = theano.tensor.cast(train_set_y, 'int32')
 test_set_y = theano.tensor.cast(test_set_y, 'int32')
 
-eta = 1.0
+eta = 0.1
 net.SGD((train_set_x, train_set_y), num_of_epochs, mini_batch_size, eta,
         (test_set_x, test_set_y), (test_set_x, test_set_y))
 
 # save results:
-#base_path = "/home/ohadsh/Dropbox/Rami/Code_ohad/outputs/NN_basic"
-#train_path = os.path.join(base_path, dict_base_name)
-#os.makedirs(train_path)
-#training_results_handler.save_net(net, os.path.join(train_path, "net.pcl"))
-#training_results_handler.save_net_params(net.sizes, len(training_data),
-#                                         len(test_data), num_of_epochs, mini_batch_size,
+base_path = '/sheard/googleDrive/Master/courses/probabilistic_graphical_models/outputs/part_3/'
+train_path = os.path.join(base_path, dict_base_name)
+os.makedirs(train_path)
+training_results_handler.save_net(net, os.path.join(train_path, "net.pcl"))
+# training_results_handler.save_net_params(net.layers, len(X_train),
+#                                         len(X_test), num_of_epochs, mini_batch_size,
 #                                     eta, net.test_results, os.path.join(train_path, "info.txt"))
